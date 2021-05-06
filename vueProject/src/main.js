23f9dd82af08d2270f18a969fb2b3d59f301b3ab
lenglengaaaaa <lenglengaaaaa@163.com>
2021-05-06 21:21:12 +0800

    mock设置

--- vueProject/src/main.js
+++ vueProject/src/main.js
@@ -4,14 +4,20 @@ import routers from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
-import env from './env'
+// import env from './env'
 
-// 根据前端的跨域方式做调整 /a/b  /api/a/b => /a/b
-axios.defaults.baseURL = '/api';
+const mock = true;
+if(mock){
+  require('./mock/api')
+}
+
+
+// 根据前端的跨域方式做调整 /a/b  / api/a/b => /a/b
+// axios.defaults.baseURL = '/api';
 axios.defaults.timeout = 8000;
 
 // 根据环境变量获取不同的请求地址
-axios.defaults.baseURL = env.baseURL;
+// axios.defaults.baseURL = env.baseURL;
 
 
 //interceptors拦截器
